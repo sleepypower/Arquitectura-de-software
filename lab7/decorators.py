502228872ef48a9e6147846480c0456466944472
David Melendez <daalmega@gmail.com>
2021-02-23 21:33:16 -0500

    Added decorator exercise

---
+++ lab7/decorators.py
@@ -0,0 +1,41 @@
+import requests
+
+
+def suma_fake(function):
+    def wrapper(num):
+        if num % 2 == 0:
+            return function(num)
+        else:
+            num = 4
+            return function(num)
+    return wrapper
+
+
+def consulta_continente(function):
+    def wrapper(continente):
+        if continente == "asia":
+            response = requests.get(f"https://restcountries.eu/rest/v2/region/{continente}")
+            print(
+                response.content
+            )
+        else:
+            return function()
+    return wrapper
+
+def formatear_base_de_datos(function):
+    def wrapper(lista):
+        if "base de datos" in lista:
+            print("Base de datos formateada!")
+            return function(lista)
+        
+        else:
+            return function()
+    return wrapper
+            
+def formatear_sistema(function):
+    def wrapper(lista):
+        if "sistema" in lista:
+            print("Solo se ha formateado el sistema")
+        else:
+            return function(lista)
+    return wrapper
---
+++ lab7/main.py
@@ -0,0 +1,40 @@
+from decorators import suma_fake, consulta_continente, formatear_base_de_datos,\
+                       formatear_sistema
+from factory import Factory
+
+
+@suma_fake
+def suma(num):
+    return 2 + num
+
+
+@suma_fake
+def suma2(num):
+    return 2 + num
+
+
+@consulta_continente
+def alerta_continente(continente=None):
+    return "Continente no soportado :("
+
+@formatear_sistema
+@formatear_base_de_datos
+def formatear(string):
+    print("Formateado base completado!")
+
+
+"""
+print(alerta_continente("europa"))
+print("=========================")
+
+print(suma(3))
+print(suma2(5))
+
+print("=========================")
+
+object_factory = Factory("Bills").get_instance()
+print(object_factory().get_factura())
+"""
+
+formatear(["base de datos", "sistema"])
+
